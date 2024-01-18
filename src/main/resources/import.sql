707999d885201317ac5aa759449baa408d6cb33d
musella95 <musellamatteoo@hotmail.com>
2024-01-18 16:39:28 +0100

    add in importSql any new ingredients whith insert into + value

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,18 @@
 insert into pizzas (price, img_url, name, description) values (5.50, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/260px-Eq_it-na_pizza-margherita_sep2005_sml.jpg", "Margherita", "Pomodoro, Fiordilatte, Basilico");
 insert into pizzas (price, img_url, name, description) values (4.50, " ", "Marinara", "Pomodoro, Origano");
 insert into offers (pizzaId, start_data, expired_data, title) values (1,'2024-01-20','2024-02-20',"margherita day")
+INSERT INTO ingredients (name) VALUES ('Sugo');
+INSERT INTO ingredients (name) VALUES ('Mozzarella');
+INSERT INTO ingredients (name) VALUES ('patatine');
+INSERT INTO ingredients (name) VALUES ('Salame');
+INSERT INTO ingredients (name) VALUES ('Spek');
+INSERT INTO ingredients (name) VALUES ('Prosciutto crudo');
+INSERT INTO ingredients (name) VALUES ('Salsiccia');
+INSERT INTO ingredients (name) VALUES ('Provola');
+INSERT INTO ingredients (name) VALUES ('Burrata');
+INSERT INTO ingredients (name) VALUES ('Mortadella');
+INSERT INTO ingredients (name) VALUES ('Pesto');
+INSERT INTO ingredients (name) VALUES ('Pesto di pistacchio');
+INSERT INTO ingredients (name) VALUES ('alici');
+INSERT INTO ingredients (name) VALUES ('datterini');
+INSERT INTO ingredients (name) VALUES ('Pomodorini');
